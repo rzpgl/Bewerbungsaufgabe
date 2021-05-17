99f3da49c15bb4384af7017cacabf1e1c7299e55
rzpgl <84333526+rzpgl@users.noreply.github.com>
2021-05-17 18:46:10 +0200

    Update Search.cs

--- Search.cs
+++ Search.cs
@@ -1,52 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using Google.Apis.Customsearch.v1;
-using Google.Apis.Customsearch.v1.Data;
-using Google.Apis.Services;
+using System;
 
 namespace BewerbungsAufgabe
+// von Alexander Dräger
 {
-    public class Search
+    public class GoogleSearch
     {
-        public void startSearch(string configApiKey, string configSearchId, string searchRequest)
-        {
-            var customSearchService = new CustomsearchService(new BaseClientService.Initializer
-            { ApiKey = configApiKey });
-            CseResource.ListRequest listRequest = customSearchService.Cse.List();
-            listRequest.Cx = configSearchId;
-            listRequest.Q = searchRequest;
-            IList<Result> resultCache = new List<Result>();
-            printSearchResultItem(resultCache, listRequest);
-        }
+        static bool newSearch = true;
 
-        private void printSearchResultItem(IList<Result> resultCache, CseResource.ListRequest listRequest)
+        static void Main(string[] args)
         {
-            const int pageSize = 10;
-            const int searchEnd = 2;
-            int pagePosition = 0;
-            int outputCounter = 0;
-
-            while (pagePosition < searchEnd)
+            GoogleSearch newProgram = new GoogleSearch();
+            RequestReader request = new RequestReader();
+            ConfigReader config = new ConfigReader();
+            config.firstRead();
+            while (newSearch)
             {
-                listRequest.Start = pagePosition * pageSize + 1;
-                resultCache = listRequest.Execute().Items;
-                try
-                {
-                    foreach (var item in resultCache)
-                    {
-                        outputCounter++;
-                        Console.WriteLine("Ergebnis: " + outputCounter + Environment.NewLine + "Titel: " + item.Title + Environment.NewLine +
-                                          "URL: " + item.Link + Environment.NewLine + "======================================================" +
-                                          Environment.NewLine);
-                    }
-                    pagePosition++;
-                }
-                catch (System.NullReferenceException n)
-                {
-                    Console.WriteLine(Environment.NewLine + "Keine Ergebnisse für diese Suchanfrage." +
-                        Environment.NewLine + "Caught: {0}", n.Message + Environment.NewLine);
-                    break;
-                }
+                Search Search = new Search();
+                Search.startSearch(config.getApiKey(), config.getSearchId(), request.ReadSearchRequest());
+                newProgram.quitProgram();
             }
         }
 
