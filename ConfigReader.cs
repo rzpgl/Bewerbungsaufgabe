b173a85a314b9866da2f780bcdd53f1ef3e7eec5
rzpgl <84333526+rzpgl@users.noreply.github.com>
2021-05-18 09:45:10 +0200

    Add files via upload

---
+++ ConfigReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace BewerbungsAufgabe
+{
+    public class ConfigReader
+    {
+        private string apiKey;
+        private string searchId;
+        private string filePath;
+
+        public string getApiKey() { return apiKey; }
+        public string getSearchId() { return searchId; }
+
+        public void firstRead()
+        {
+            this.getPath();
+            Console.WriteLine("config wird eingelesen..." + Environment.NewLine);
+            this.readConfig();
+        }
+
+        private void getPath()
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = "config.txt";
+            filePath = Path.GetFullPath(Path.Combine(path, @"../../../", filename));
+        }
+
+        private void readConfig()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                apiKey = lines[0];
+                searchId = lines[1];
+                Console.WriteLine("config erfolgreich eingelesen." + Environment.NewLine);
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine("Config ist nicht im entsprechenden Verzeichnis. Bitte in Projektordner kopieren." +
+                    Environment.NewLine + "Caught: {0}", e.Message);
+                Environment.Exit(1);
+            }
+            catch (System.IndexOutOfRangeException i)
+            {
+                Console.WriteLine("Config konnte nicht eingelesen werden, da diese leer ist. Bite Datei kontrollieren." +
+                    Environment.NewLine + "Caught: {0}", i.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+
+namespace BewerbungsAufgabe
+{
+    public class Search
+    {
+        private int outputCounter = 0;
+
+
+        public void startSearch(string configApiKey, string configSearchId, string searchRequest)
+        {
+            var customSearchService = new CustomsearchService(new BaseClientService.Initializer
+            { ApiKey = configApiKey });
+            CseResource.ListRequest listRequest = customSearchService.Cse.List();
+            listRequest.Cx = configSearchId;
+            listRequest.Q = searchRequest;
+            IList<Result> resultCache = new List<Result>();
+            printSearchResultItems(resultCache, listRequest);
+        }
+
+        private void printSearchResultItems(IList<Result> resultCache, CseResource.ListRequest listRequest)
+        {
+            const int pageSize = 10;
+            const int searchEnd = 2;
+            int pagePosition = 0;
+
+            while (pagePosition < searchEnd)
+            {
+                listRequest.Start = pagePosition * pageSize + 1;
+                resultCache = listRequest.Execute().Items;
+                this.printItems(resultCache);
+                pagePosition++;
+            }
+        }
+
+
+        private void printItems(IList<Result> resultCache)
+        {
+            try
+            {
+                foreach (var item in resultCache)
+                {
+                    outputCounter++;
+                    Console.WriteLine("Ergebnis: " + outputCounter + Environment.NewLine + "Titel: " + item.Title + Environment.NewLine +
+                                                  "URL: " + item.Link + Environment.NewLine + "======================================================" +
+                                                  Environment.NewLine);
+                }
+            }
+            catch (System.NullReferenceException n)
+            {
+                Console.WriteLine(Environment.NewLine + "Keine Ergebnisse für diese Suchanfrage." +
+                    Environment.NewLine + "Caught: {0}", n.Message + Environment.NewLine);
+            }
+        }
+    }
+}
